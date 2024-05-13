5177bac9e9bed148bc60630a39fe27ccad9fcce7
Josh Robson Chase <josh@robsonchase.com>
2024-05-13 18:21:44 -0400

    bevy_async_util: lint fixes

--- bevy_async_util/src/nohup.rs
+++ bevy_async_util/src/nohup.rs
@@ -1,39 +1,45 @@
-use std::future::Future;
-use std::pin::Pin;
-use std::task::Context;
-use std::task::Poll;
+use std::{
+  future::Future,
+  pin::Pin,
+  task::{
+    Context,
+    Poll,
+  },
+};
 
-use bevy::tasks::IoTaskPool;
-use bevy::tasks::Task;
+use bevy::tasks::{
+  IoTaskPool,
+  Task,
+};
 
 pub struct NoHup<T>(Option<Task<T>>);
 
 impl<T> NoHup<T> {
-    pub fn new(task: Task<T>) -> Self {
-        NoHup(Some(task))
-    }
+  pub fn new(task: Task<T>) -> Self {
+    NoHup(Some(task))
+  }
 }
 
 impl<T> From<Task<T>> for NoHup<T> {
-    fn from(value: Task<T>) -> Self {
-        Self::new(value)
-    }
+  fn from(value: Task<T>) -> Self {
+    Self::new(value)
+  }
 }
 
 impl<T> Future for NoHup<T> {
-    type Output = T;
+  type Output = T;
 
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        Pin::new(&mut self.0.as_mut().expect("poll after drop")).poll(cx)
-    }
+  fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    Pin::new(&mut self.0.as_mut().expect("poll after drop")).poll(cx)
+  }
 }
 
 impl<T> Drop for NoHup<T> {
-    fn drop(&mut self) {
-        self.0.take().expect("nohup drop once").detach()
-    }
+  fn drop(&mut self) {
+    self.0.take().expect("nohup drop once").detach()
+  }
 }
 
 pub fn nohup<T: Send + 'static>(f: impl Future<Output = T> + Send + 'static) -> NoHup<T> {
-    NoHup::new(IoTaskPool::get().spawn(f))
+  NoHup::new(IoTaskPool::get().spawn(f))
 }
